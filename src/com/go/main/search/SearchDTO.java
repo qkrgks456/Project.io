3cf8e465874c9353eac69340e90d3230eb5d425d
DESKTOP-9U1R59V\GDJ36 <sunuiid@naver.com>
2021-07-28 16:13:18 +0900

    이선우 테스트

--- src/com/go/main/search/SearchDTO.java
+++ src/com/go/main/search/SearchDTO.java
@@ -2,5 +2,11 @@ package com.go.main.search;
 
 public class SearchDTO {
 	
+	private String cafeKey;
+	private String ownerNo;
+	private String cafeName;
+	private String Confusion;
+	private int likeNo;
+	
 	
 }
